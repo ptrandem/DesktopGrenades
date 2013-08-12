06774c2a12f10b70a8d66345ddeb396c7b78cdd7
Paul Trandem <ptrandem@gmail.com>
2013-08-11 22:16:07 -0500

    changed default index.html url to derive from source host; added logic to output client id

--- SocketServer/controller.js
+++ SocketServer/controller.js
@@ -21,11 +21,12 @@ function handler (req, res) {
 }
 
 io.sockets.on('connection', function (socket) {
+  
   socket.emit('news', { hello: 'world' });
   socket.on('my other event', function (data) {
     console.log(data);
   });
   socket.on('checkin', function(data){
-  	console.log("check in from " + data.checkin);
+  	console.log("check in from " + data.checkin + " (client: " + socket.id + ")");
   });
 });
